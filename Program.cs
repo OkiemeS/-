35c612080894861b87ca63a4661fbaae1bcc7e4a
Павел Семейко <pawel.semeiko@yandex.ru>
2021-10-03 15:31:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Банк
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User kityskin = new User("Павел", "Семейко", 24, 1000999);
+            Console.WriteLine($"Здравствуйте {kityskin.FulName} \n Ваши доходы составляют {kityskin.Doxod} укажите сумму вклада");
+            string sum = Console.ReadLine();
+            double summa = Convert.ToInt32(sum);
+            Console.WriteLine(" Укажите на какой срок времени хотите совершить вклад.");
+            string tim = Console.ReadLine();
+            int timme = Convert.ToInt32(tim);
+            double fulMonney = User.Fulmanney(summa, timme, kityskin);
+            double chistka = fulMonney - summa;
+            Console.WriteLine($" Уважаемый клиент, совершив вклад размером {summa} сроком на {timme} вы сможете снять {fulMonney}" +
+                $" \n Чистая прибыль составит {chistka} \n " +
+                $"\n Благодарим за то что воспользовались нашим Сервисом, надеемся на дальнеейшее сотрудничество." +
+                $" Всем спасибо, и  пошли нафиг");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Банк
+{
+    class User
+    {
+        public string name;
+        public string Name
+        {
+            set { name = value; } 
+        }
+        public string family;
+        public string Family 
+        { 
+            set { family = value;} 
+        }
+        public string FulName
+        {
+            get { return $" {name} {family}"; }
+        }
+        public byte age;
+        public byte Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new Exception(" Некоректно заданный возвраст клиента");
+                }
+            }
+        }
+
+        public double Doxod { get; set; }
+
+        public User(string name, string family, byte age, double doxod)
+        {
+            Name = name;
+            Family = family;
+            Age = age;
+            Doxod = doxod;
+        }
+        public readonly double minWklad = 1000;
+        public readonly int minTime = 1;
+        private double rekaficientSum(double doxod, User user)
+        {
+            if ( doxod <= minWklad) throw new Exception ("недопустимая сумма ");
+            if (doxod > user.Doxod) throw new Exception(" Сумма вклада больше имеющейся суммы денег на счету Клиента");
+            if (doxod <= 10000)
+            {
+                return 1.05;
+            }
+            if (doxod <= 100000 && doxod > 10000)
+            {
+                return  1.1;
+            }
+            else
+            {
+                return 1.12;
+            }
+        }
+        private double kaficientAge(int age)
+        {
+            if (age < minTime) throw new Exception("минимальный период времени 1 год ");
+            if (age <= 2)
+            {
+                return 1.05;
+            }
+            if (age <= 5 && age > 2)
+            {
+                return 1.1;
+            }
+            else
+            {
+                return 1.12;
+            }
+        }
+        public static double Fulmanney(double summa, int time, User user)
+        {
+            return summa * user.rekaficientSum(summa, user) * user.kaficientAge(time);
+        }
+    }
+}
